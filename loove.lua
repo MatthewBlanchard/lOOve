66a79faf67d9f1e848227afe467ede6cb172b9dd
Matthew Blanchard <matthewblanc@msn.com>
2015-11-18 01:10:34 -0500

    Updated for love 0.10

--- loove.lua
+++ loove.lua
@@ -1,52 +1,54 @@
+function love.state(o)
+    love.state = o
+end
+
 function love.run()
 
-    assert(love.s, "Please call love.state with a gamestate object.")
-    
-    math.randomseed(os.time())
-    math.random() math.random()
+    assert(love.state, "Please call love.state with a gamestate object.")
 
-    if love.s.load then love.load(arg) end
+    if love.math then
+        love.math.setRandomSeed(os.time())
+    end
+
+    if love.state.load then love.state:load(arg) end
+
+    -- We don't want the first frame's dt to include time taken by love.load.
+    if love.timer then love.timer.step() end
 
     local dt = 0
 
+    -- Main loop time.
     while true do
         -- Process events.
         if love.event then
             love.event.pump()
-            for e,a,b,c,d in love.event.poll() do
-                if e == "quit" then
-                    if not love.s.quit or not love.s:quit() then
-                        if love.audio then
-                            love.audio.stop()
-                        end
-                        return
+            for name, a,b,c,d,e,f in love.event.poll() do
+                if name == "quit" then
+                    if not love.state.quit or not love.state:quit() then
+                        return a
                     end
                 end
-                
-                if love.s[e] then
-                    love.s[e](love.s,a,b,c,d)
-                end
+                love.state[name](a,b,c,d,e,f)
             end
         end
 
+        -- Update dt, as we'll be passing it to update
         if love.timer then
             love.timer.step()
             dt = love.timer.getDelta()
         end
 
-        if love.s.update then love.s:update(dt) end
-        if love.graphics then
-            love.graphics.clear()
-            if love.s.draw then love.s:draw() end
+        -- Call update and draw
+        if love.state.update then love.state:update(dt) end -- will pass 0 if love.timer is disabled
+
+        if love.graphics and love.graphics.isActive() then
+            love.graphics.clear(love.graphics.getBackgroundColor())
+            love.graphics.origin()
+            if love.state.draw then love.state:draw() end
+            love.graphics.present()
         end
 
         if love.timer then love.timer.sleep(0.001) end
-        if love.graphics then love.graphics.present() end
-
     end
 
-end
-
-function love.state(o)
-    love.s = o
 end
